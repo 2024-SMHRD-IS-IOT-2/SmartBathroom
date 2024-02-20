1b8515f6725a019c4c01f6ace7ce5c38a34f6245
sjk <jaykim.coding@gmail.com>
2024-02-20 15:20:16 +0900

    첫번째 커밋

---
+++ react-project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>핵심프로젝트-스마트화장실</h1>
+      <h2>추가</h2>
+
+    </div>
+  );
+}
+
+export default App;

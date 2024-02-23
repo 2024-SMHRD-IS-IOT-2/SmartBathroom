c7b154d7a74202a422fc76f14e2e9f6067fdeee5
muk <wow159456@gmail.com>
2024-02-23 12:20:22 +0900

    12:20

--- react-project/src/pages/HomePage.jsx
+++ react-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import img from './siout.png'; // 이미지를 임포트합니다.
+import img from '../mideo/siout.png'; // 이미지를 임포트합니다.
+import video from '../mideo/video.mp4';
 
 function HomePage() {
   const navigate = useNavigate();
@@ -10,34 +11,36 @@ function HomePage() {
   };
 
   return (
-    <div style={{ maxWidth: '100%', margin: '0 auto', paddingBottom: '20px' }}> {/* 하단 여백 추가 */}
-      {/* 이미지 */}
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', maxWidth: '1200px', margin: '0 auto' }}>
-        <img
-          src={img} 
-          alt=""
-          style={{ width: '20%', height: 'auto' }} // 이미지 크기를 조정합니다.
+    <div style={{ position: 'fixed', top: 0, left: 0, right: 0 }}> {/* 이미지와 로그인 버튼이 함께 스크롤에 따라 이동하도록 부모 컨테이너에 position: fixed 적용 */}
+      <div style={{ position: 'relative', maxWidth: '100%', margin: '0 auto', paddingBottom: '20px' }}> {/* 이미지와 비디오 컨테이너에 position: relative 추가 */}
+        {/* 이미지 */}
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', maxWidth: '1200px', margin: '0 auto' }}>
+          <img
+            src={img} 
+            alt=""
+            style={{ width: '20%', height: 'auto' }} // 이미지 크기 조정
+          />
+          <div style={{ marginLeft: '20px' }}>
+            <h1>이미지를 눌러 제품 정보를 확인하세요!</h1>
+            <p>더 많은 정보를 원하시면 클릭해주세요.</p>
+          </div>
+        </div> 
+    
+        {/* 시연 영상 */}
+        <video
+          src={video}
+          controls
+          style={{ width: '100%', height: 'auto' }} // 비디오 크기 조정
         />
-        <div style={{ marginLeft: '20px' }}>
-          <h1>이미지를 눌러 제품 정보를 확인하세요!</h1>
-          <p>더 많은 정보를 원하시면 클릭해주세요.</p>
+
+        <div style={{backgroundColor: 'blue', height:'100px'}}>
+          하단배너
         </div>
-      </div> 
-
-      {/* 시연 영상 */}
-      <iframe
-        width="100%"
-        height="500"
-        src="https://www.youtube.com/embed/VIDEO_ID" // 영상 링크 -추가로 수정해야함
-        title="시연 영상"
-        frameBorder="0"
-        allowFullscreen
-        style={{ maxWidth: '100%', marginTop: '20px' }} // 화면 크기에 따라 영상 크기 조정 
-      ></iframe>
-
-      {/* 로그인 버튼을 우측 상단으로 이동 */}
-      <div style={{ position: 'absolute', top: '20px', right: '20px' }}>
-        <button onClick={goToLoginPage} style={{ padding: '20px 55px', fontSize: '16px' }}>로그인</button>
+    
+        {/* 로그인 버튼을 우측 상단으로 이동 */}
+        <div style={{ position: 'fixed', top: '20px', right: '20px', zIndex: 999 }}> {/* position: fixed를 사용하여 화면을 스크롤해도 고정됩니다. */}
+          <button onClick={goToLoginPage} style={{ padding: '20px 55px', fontSize: '16px' }}>로그인</button>
+        </div>
       </div>
     </div>
   );

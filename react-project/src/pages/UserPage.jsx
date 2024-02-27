80b299d21d17b23b936aa3e122a7278becdb66fc
cki <kisahs@gmail.com>
2024-02-27 09:07:36 +0900

    그래프

--- react-project/src/pages/UserPage.jsx
+++ react-project/src/pages/UserPage.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from '../axios';
-import { UserContext } from '../contexts/UserContext';
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "../axios";
+import { UserContext } from "../contexts/UserContext";
+import Charts from "../components/Chart";
 
 // 삼성 갤럭시 알람 형식으로 시간을 변환하는 함수
 const convertToGalaxyAlarmFormat = (time) => {
@@ -15,24 +16,24 @@ const UserPage = () => {
   const [userAccidents, setUserAccidents] = useState([]);
   const [isEditing, setIsEditing] = useState({ editing: false, acc_idx: "" });
   const editInfoRef = useRef(null);
-  const { isLoggedin, loginData } = useContext(UserContext); 
-
+  const { isLoggedin, loginData } = useContext(UserContext);
 
   const fetchAccidents = async () => {
-          try {
-            const response = await axios.post('/user/showAccident', { userId: loginData.member_id });
-            if (response.data.result === "success") {
-              setUserAccidents(response.data.rows);
-            } else {
-              setUserAccidents([]);
-              console.log("사고이력 없음.");
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        };
+    try {
+      const response = await axios.post("/user/showAccident", {
+        userId: loginData.member_id,
+      });
+      if (response.data.result === "success") {
+        setUserAccidents(response.data.rows);
+      } else {
+        setUserAccidents([]);
+        console.log("사고이력 없음.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  
   // useEffect(() => {
   //   console.log("isLoggedin", isLoggedin);
   //   if (!isLoggedin) {
@@ -40,21 +41,20 @@ const UserPage = () => {
   //     navigate('/');
   //   } else {
 
-      //     fetchAccidents();
-      //   }
-      // }, [isLoggedin, navigate, loginData.member_id]);
-
+  //     fetchAccidents();
+  //   }
+  // }, [isLoggedin, navigate, loginData.member_id]);
 
   const goToChartPage = () => {
-    navigate('/chart');
+    navigate("/chart");
   };
 
   const goToChangeUiPage = () => {
-    navigate('/changeui', { state: { data: loginData, from: "user" } });
+    navigate("/changeui", { state: { data: loginData, from: "user" } });
   };
 
   const saveSettings = () => {
-    console.log('Settings saved!');
+    console.log("Settings saved!");
   };
 
   const updateAccInfo = (accident) => {
@@ -66,60 +66,153 @@ const UserPage = () => {
   };
 
   return (
-    <div style={{ display: 'flex', alignItems: 'flex-start', height: '100vh', width:"100%",  padding: '20px', position: 'relative' }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "flex-start",
+        height: "100vh",
+        width: "100%",
+        padding: "20px",
+        position: "relative",
+      }}
+    >
       {/* 왼쪽 : 회원 정보창 */}
-      <div style={{ display: 'flex', flexDirection: 'column', width: '33%', alignItems: 'center', marginBottom: '20px' }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "33%",
+          alignItems: "center",
+          marginBottom: "20px",
+        }}
+      >
         <h1>{loginData.member_id}님의 정보</h1>
-        <div style={{ textAlign: 'center', width: '100%' }}>
-          <p style={{ fontSize: '1.5em', marginBottom: '10px' }}>이름: {loginData.member_name}</p>
-          <p style={{ fontSize: '1.5em', marginBottom: '10px' }}>보호자 이름: {loginData.guardian_name}</p>
-          <p style={{ fontSize: '1.5em', marginBottom: '10px' }}>수면 시간: {convertToGalaxyAlarmFormat(`${sleepTime}:00`)} 시간</p>
-          <input type="range" min="4" max="12" value={sleepTime} onChange={(e) => setSleepTime(e.target.value)} style={{ width: '80%', marginBottom: '20px' }} />
-          <p style={{ fontSize: '1.5em', marginBottom: '10px' }}>조명 밝기: {sleepLightening}%</p>
-          <input type="range" min="0" max="100" value={sleepLightening} onChange={(e) => setSleepLightening(e.target.value)} style={{ width: '80%' }} />
-          <button onClick={saveSettings} style={{ fontSize: '1em', marginTop: '20px' }}>설정 저장</button>
-        <button onClick={goToChangeUiPage} style={{ fontSize: '1em', marginBottom: '10px' }}>회원정보 변경</button>
+        <div style={{ textAlign: "center", width: "100%" }}>
+          <p style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+            이름: {loginData.member_name}
+          </p>
+          <p style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+            보호자 이름: {loginData.guardian_name}
+          </p>
+          <p style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+            수면 시간: {convertToGalaxyAlarmFormat(`${sleepTime}:00`)} 시간
+          </p>
+          <input
+            type="range"
+            min="4"
+            max="12"
+            value={sleepTime}
+            onChange={(e) => setSleepTime(e.target.value)}
+            style={{ width: "80%", marginBottom: "20px" }}
+          />
+          <p style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+            조명 밝기: {sleepLightening}%
+          </p>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={sleepLightening}
+            onChange={(e) => setSleepLightening(e.target.value)}
+            style={{ width: "80%" }}
+          />
+          <button
+            onClick={saveSettings}
+            style={{ fontSize: "1em", marginTop: "20px" }}
+          >
+            설정 저장
+          </button>
+          <button
+            onClick={goToChangeUiPage}
+            style={{ fontSize: "1em", marginBottom: "10px" }}
+          >
+            회원정보 변경
+          </button>
         </div>
       </div>
-  
+
       {/* 중앙 : 사고 이력 */}
-      <div style={{ width: '33%', marginLeft: '20px' }}> {/* 좌측 정보창과 간격을 두기 위해 marginLeft 추가 */}
+      <div style={{ width: "33%", marginLeft: "20px" }}>
+        {" "}
+        {/* 좌측 정보창과 간격을 두기 위해 marginLeft 추가 */}
         <h1>사고 이력</h1>
         <ul>
           {userAccidents.map((accident, index) => (
-            <li key={index} className={`${accident.acc_status === "Y" ? 'admin-modal-item-hl' : ''}`} style={{ marginBottom: '20px' }}>
+            <li
+              key={index}
+              className={`${
+                accident.acc_status === "Y" ? "admin-modal-item-hl" : ""
+              }`}
+              style={{ marginBottom: "20px" }}
+            >
               <div style={{ display: "flex", alignItems: "center" }}>
-                <div style={{ width: "350px", marginRight: '20px' }}>
-                  <p>발생 시간: {new Date(accident.acc_time).toLocaleString()}</p>
+                <div style={{ width: "350px", marginRight: "20px" }}>
+                  <p>
+                    발생 시간: {new Date(accident.acc_time).toLocaleString()}
+                  </p>
                   <p>사고 정보: {accident.acc_info}</p>
                 </div>
                 <div>
-                  {isEditing.editing && (isEditing.acc_idx === accident.acc_idx) ? (
+                  {isEditing.editing &&
+                  isEditing.acc_idx === accident.acc_idx ? (
                     <div style={{ display: "flex", alignItems: "center" }}>
                       <input
-                        type='text'
+                        type="text"
                         defaultValue={accident.acc_info}
                         ref={editInfoRef}
                       />
-                      <span className="admin-modal-btn-edit" onClick={() => { updateAccInfo(accident) }}>✔</span>
-                      <span className="admin-modal-btn-edit" onClick={() => { setIsEditing({ editing: false, acc_idx: "" }) }}>✖</span>
+                      <span
+                        className="admin-modal-btn-edit"
+                        onClick={() => {
+                          updateAccInfo(accident);
+                        }}
+                      >
+                        ✔
+                      </span>
+                      <span
+                        className="admin-modal-btn-edit"
+                        onClick={() => {
+                          setIsEditing({ editing: false, acc_idx: "" });
+                        }}
+                      >
+                        ✖
+                      </span>
                     </div>
                   ) : (
-                      <span className="admin-modal-btn-edit" onClick={() => { setIsEditing({ editing: true, acc_idx: accident.acc_idx }) }}>🖊</span>
-                    )}
-                  {accident.acc_status === "Y" &&
-                    (<button className="admin-modal-btn-edit" onClick={() => { updateAccident(accident) }}>해결</button>)}
+                    <span
+                      className="admin-modal-btn-edit"
+                      onClick={() => {
+                        setIsEditing({
+                          editing: true,
+                          acc_idx: accident.acc_idx,
+                        });
+                      }}
+                    >
+                      🖊
+                    </span>
+                  )}
+                  {accident.acc_status === "Y" && (
+                    <button
+                      className="admin-modal-btn-edit"
+                      onClick={() => {
+                        updateAccident(accident);
+                      }}
+                    >
+                      해결
+                    </button>
+                  )}
                 </div>
               </div>
             </li>
           ))}
         </ul>
       </div>
-  
+
       {/* 오른쪽 : 차트 */}
-      <div style={{ width: '33%', flex: "1" }}>
+      <div style={{ width: "33%", flex: "1" }}>
         <h1>차트</h1>
         {/* 차트를 표시하는 코드 */}
+        <Charts />
       </div>
     </div>
   );
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../config/database");
 
-//default 값 : DB에 등록해서 주석처리
-// const sleep_time = "23:00_06:00";
-// const sleep_lightening = 50;
-
 // 회원가입 라우터
 router.post("/handleJoin", (req, res) => {
   console.log("user.js 회원가입 요청...", req.body);
@@ -82,11 +78,11 @@ router.post("/handleLogin", (req, res) => {
       // 관리자 로그인
       if (userId === "admin") {
         console.log("admin입니다");
-        res.json({ result: "admin" , data : rows[0]});
+        res.json({ result: "admin", data: rows[0] });
       } else {
         // 회원 로그인
         console.log("회원입니다");
-        res.json({ result: "success" , data : rows[0] });
+        res.json({ result: "success", data: rows[0] });
       }
     } else {
       // 로그인 실패
@@ -103,9 +99,9 @@ router.get("/getSession", (req, res) => {
 });
 
 // 로그아웃 라우터
-router.post('/handleLogout', (req, res)=>{
+router.post("/handleLogout", (req, res) => {
   req.session.destroy();
-  res.json({result : 'success'})
+  res.json({ result: "success" });
 });
 
 // 개인정보 수정(ChangeUi)
@@ -170,6 +166,21 @@ router.post("/handleSleep", (req, res) => {
   });
 });
 
+//차트페이지 접속시?
+router.post("/handleChartPage", (req, res) => {
+  const { userId } = req.body;
+  const sql = `select * from sensors where member_id=?`;
+  conn.query(sql, [userId], (err, rows) => {
+    if (rows.length > 0) {
+      console.log("success", rows);
+      res.json({ rows: rows, result: "success" });
+    } else {
+      console.log("fail");
+      console.log("err:", err);
+    }
+  });
+});
+
 //관리자 페이지 : 열람기능
 router.post("/showList", (req, res) => {
   const sql = `Select *
@@ -184,14 +195,13 @@ router.post("/showList", (req, res) => {
     }
   });
 });
-
 //관리자 페이지 : 현재 사고현황이 "Y" 인 유저만 반환
-router.post("/updateAccidentStatus", (req, res)=>{
+router.post("/updateAccidentStatus", (req, res) => {
   const sql = `select distinct member_id from accidents where acc_status = "Y"`;
-  conn.query(sql, (err,rows)=>{
-    res.json({result: "success", rows : rows});
-  })
-})
+  conn.query(sql, (err, rows) => {
+    res.json({ result: "success", rows: rows });
+  });
+});
 
 //사고 정보 보내기
 router.post("/showAccident", (req, res) => {
@@ -199,7 +209,6 @@ router.post("/showAccident", (req, res) => {
   const sql = `select * from accidents where member_id=?`;
   conn.query(sql, [userId], (err, rows) => {
     if (rows.length > 0) {
-      // console.log(rows);
       res.json({ rows: rows, result: "success" });
       console.log("user.js 관리자가 열람할 사고 정보 보냈습니다.");
     } else {
@@ -210,26 +219,30 @@ router.post("/showAccident", (req, res) => {
 });
 
 //사고 정보 완료처리
-router.post("/updateAccident", (req,res)=>{
-  const {acc_idx, acc_status, acc_info} = req.body;
+router.post("/updateAccident", (req, res) => {
+  const { acc_idx, acc_status, acc_info } = req.body;
   console.log(`pdateAccident ${acc_idx}, ${acc_info}, ${acc_status}`);
 
-  const sql = `update accidents set acc_info=?, acc_status=? where acc_idx=?`
+  const sql = `update accidents set acc_info=?, acc_status=? where acc_idx=?`;
 
-  conn.query(sql, [acc_info, acc_status, acc_idx], (err, result) =>{
+  conn.query(sql, [acc_info, acc_status, acc_idx], (err, result) => {
     console.log(result);
     if (result.changedRows > 0) {
       res.json({ result: "success" });
       console.log("user.js 사고정보 수정 완료", result.changedRows);
     } else {
       console.log("err:", err);
+
       res.json({ result: "fail" });
       console.log("user.js 사고정보 수정 실패");
     }
-  })
-
-})
-
+  });
+});
+//로그아웃 기능
+router.get("/handleLogout", (req, res) => {
+  req.session.destroy();
+  res.json({ result: "success" });
+});
 
 // 아두이노 데이터 받기 skeleton code
 /*
@@ -238,7 +251,8 @@ router.post("/updateAccident", (req,res)=>{
 */
 router.get("/sensorData", (req, res) => {
   console.log("receiving data");
-  const {humidity,temp, nh3, meth, btnEmerg, falldown, member_id} = req.query;
+  const { humidity, temp, nh3, meth, btnEmerg, falldown, member_id } =
+    req.query;
 
   console.log("Received sensor data from Arduino:");
   console.log(req.query);
@@ -246,47 +260,191 @@ router.get("/sensorData", (req, res) => {
   // 받은 아이디값으로 DB 저장
   const sql = `insert into sensors(sensor_humid, sensor_temp, sensor_nh3, member_id) 
                 values (?,?,?,?)`;
-  conn.query( sql,[humidity, temp, nh3, member_id],
-    (err, rows) => {
-      if (rows) {
-        console.log(`user.js 센서저장 성공.  아이디: ${member_id}`);
-      } else {
-        console.log("user.js 센서저장실패", err);
-      }
+  conn.query(sql, [humidity, temp, nh3, member_id], (err, rows) => {
+    if (rows) {
+      console.log(`user.js 센서저장 성공.  아이디: ${member_id}`);
+    } else {
+      console.log("user.js 센서저장실패", err);
     }
-  );
+  });
   // 아두이노로 값 보내기.
+
   const sql2 = `select sleep_time, sleep_lightening from members where member_id=(?)`;
-  conn.query(sql2, [member_id],
-    (err, rows) => {
-      if (rows) {
-        // 수면시간 계산
-        let dateTime = new Date();
-        let startTime = rows[0].sleep_time.slice(0,5);
-        let endTime = rows[0].sleep_time.slice(6);
-        let isSleepLight = false;
-        
-        startTime = parseInt(startTime.replace(":",""));
-        endTime = parseInt(endTime.replace(":",""));
-        let curTime = dateTime.getHours() * 100 + dateTime.getMinutes();
-      
-        if (curTime > startTime || curTime < endTime) isSleepLight = true;
-        else isSleepLight = false; 
-        
+  conn.query(sql2, [member_id], (err, rows) => {
+    if (rows) {
+      // 수면시간 계산
+      let dateTime = new Date();
+      let startTime = rows[0].sleep_time.slice(0, 5);
+      let endTime = rows[0].sleep_time.slice(6);
+      let isSleepLight = false;
 
-        //수면등 여부 // 수면등 밝기 아두이노로 전송.
-        res.send(`${isSleepLight}*${rows[0].sleep_lightening}*`);
-        
-      } else {
-        console.log("user.js 센서값 업데이트 실패", err);
-      }
+      startTime = parseInt(startTime.replace(":", ""));
+      endTime = parseInt(endTime.replace(":", ""));
+      let curTime = dateTime.getHours() * 100 + dateTime.getMinutes();
 
+      if (curTime > startTime || curTime < endTime) isSleepLight = true;
+      else isSleepLight = false;
+
+      //수면등 여부 // 수면등 밝기 아두이노로 전송.
+      res.send(`${isSleepLight}*${rows[0].sleep_lightening}*`);
+    } else {
+      console.log("user.js 센서값 업데이트 실패", err);
     }
-  );
+  });
 
-  res.send('Data received successfully');
-
+  res.send("Data received successfully");
 });
 
-
 module.exports = router;
+
+//////////////////이 이하는 작업중 내지 더미////////////
+
+// //회원정보 기능
+// router.get("/showMember", (req, res) => {
+//   console.log("showMember data", req.query);
+//   //if (req.query.userId !== "admin") {
+//   if (userId !== "admin") {
+//     //관리자 아닐 경우 = 특정 회원만
+//     const sql = `select * from members
+//           where member_id =?`;
+//     //conn.query(sql, [req.query.userId], (err, rows) => {
+//     conn.query(sql, ["1234"], (err, rows) => {
+//       console.log("err", err);
+//       console.log("rows", rows);
+//       res.json({ result: rows });
+//     });
+//   } else {
+//     //관리자 = 전체회원 검색
+//     const sql = `select * from members where member_id != "admin"`;
+//     conn.query(sql, (err, rows) => {
+//       console.log("err", err);
+//       console.log("rows", rows);
+//       res.json({ result: rows });
+//     });
+//   }
+// });
+
+// // 탈퇴 라우터
+// // DB연동 추가
+// //회원탈퇴 기능
+// router.post("/handleDelete", (req, res) => {
+//   console.log("delete data", req.body);
+//   const { userId, userPw } = req.body;
+//   const sql = `delete from members where member_id=? and member_pw=?`;
+//   conn.query(sql, [userId, userPw], (err, rows) => {
+//     console.log("rows", rows);
+//     if (rows.affectedRows > 0) {
+//       res.redirect("/");
+//     } else {
+//       res.send(`<script>
+//             alert('존재하지 않는 회원정보입니다.');
+//             location.href='/delete';
+//             </script>`);
+//     }
+//   });
+// });
+
+// 차트 데이터 조회 라우터
+// DB 연동 코드 추가
+
+// 차트 UI페이지 차트 유형
+// 유형 ,저장 및 공유
+// DB 연동 코드 추가
+
+// router.post('/select', (req, res) => {
+// // 회원 정보 리스트
+//     // DB 연동 코드 추가
+//         // => 더미 데이터
+//     let rows = [
+//         { id: 'test1', name: '유저 1' },
+//         { id: 'test2', name: '유저 2' },
+//         { id: 'test3', name: '유저 3' },
+//         { id: 'test4', name: '유저 4' },
+//         { id: 'test5', name: '유저 5' },
+//         { id: 'test6', name: '유저 6' },
+//         { id: 'test7', name: '유저 7' },
+//         { id: 'test8', name: '유저 8' },
+//         { id: 'test9', name: '유저 9' },
+//         { id: 'test10', name: '유저 10' }
+//     ];
+
+//     res.json({ rows: rows });
+// });
+
+// // 개인정보 수정(ChangeUi) - selectedId 있는 버전
+// router.post("/handleModify", (req, res) => {
+//   console.log("Modify Member Info", req.body);
+//   const {
+//     userPw,
+//     userNumber,
+//     addr,
+//     height,
+//     weight,
+//     guardianName,
+//     guardianNumber,
+//     userId,
+//     selectedId,
+//   } = req.body;
+//   //일반 회원
+//   if (!selectedId) {
+//   const sql = `UPDATE members
+//                  SET member_pw = ?, member_phone = ?, member_addr = ?,
+//                      member_height = ?, member_weight = ?, guardian_name = ?,
+//                      guardian_phone = ?
+//                  WHERE member_id = ?`;
+//   conn.query(
+//     sql,
+//     [
+//       userPw,
+//       userNumber,
+//       addr,
+//       height,
+//       weight,
+//       guardianName,
+//       guardianNumber,
+//       userId,
+//     ],
+//     (err, result) => {
+//       console.log(result);
+//       if (result.changedRows > 0) {
+//         res.json({ result: "success" });
+//         console.log("user.js 회원정보 수정 완료");
+//       } else {
+//         console.log("err:", err);
+//         res.json({ result: "fail" });
+//         console.log("user.js 회원정보 수정 실패");
+//       }
+//     }
+//   );
+//   관리자
+//   } else {
+//     const sql = `UPDATE members
+//                  SET member_pw = ?, member_phone = ?, member_addr = ?,
+//                      member_height = ?, member_weight = ?, guardian_name = ?,
+//                      guardian_phone = ? where member_id=?`;
+//     conn.query(
+//       sql,
+//       [
+//         userPw,
+//         userNumber,
+//         addr,
+//         height,
+//         weight,
+//         guardianName,
+//         guardianNumber,
+//         selectedId,
+//       ],
+//       (err, result) => {
+//         console.log(result);
+//         if (result.changedRows > 0) {
+//           res.json({ result: "success" });
+//           console.log("user.js 회원정보 수정 완료 관리자");
+//         } else {
+//           console.log("err:", err);
+//           res.json({ result: "fail" });
+//           console.log("user.js 회원정보 수정 실패 관리자");
+//         }
+//       }
+//     );
+//   }
+// });
